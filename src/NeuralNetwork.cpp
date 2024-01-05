baffc923e6c562eef3e6634f93625b1ef54855e0
Taj Brar <taj.brar3047@gmail.com>
2024-01-04 21:59:04 -0600

    Added bias neurons

--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -1,10 +1,11 @@
 #include "NeuralNetwork.h"
 
 NeuralNetwork::NeuralNetwork(const std::vector<int> &topology) {
+    // create layers with bias neuron
     for (int i = 0; i < topology.size(); i++) {
         // get num of neuron in prev layer
-        int num_neurons_prev = i == 0 ? 0 : topology[i - 1];
-        this->layers.emplace_back(topology[i], num_neurons_prev);
+        int num_neurons_prev = i == 0 ? 0 : topology[i - 1] + 1;
+        this->layers.emplace_back(topology[i] + 1, num_neurons_prev);
     }
 }
 
